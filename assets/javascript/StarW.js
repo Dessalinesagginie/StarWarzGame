94c5d17456b2f43a53ccd2995a589ca1300b576c
DessalinesAgginie <Dessagginie@gmail.com>
2023-12-03 07:02:48 -0600

    OMO still

--- assets/javascript/StarW.js
+++ assets/javascript/StarW.js
@@ -1,108 +1,59 @@
-let yourCharacter
+$(document).ready(function() {
+  let yourCharacter = {
 
-let yourOpponent 
-
-let ahsoka = {
-  name: "Ahsoka",
-  healthPoints: 165,
-  attackPower:  15,
-  counterAttackPower:125
-}
-
-let kyloRen = {
-  name: "Kylo-Ren",
-  healthPoints: 175,
-  attackPower: 150,
-  counterAttackPower: 125
-  
-}
-
-let rey = {
-  name: "Rey",
-  healthPoints: 160,
-  attackPower: 150,
-  counterAttackPower: 125
-}
-
-
-let yoda = {
-  name: "Yoda",
-  healthPoints: 190,
-  attackPower: 160,
-  counterAttackPower: 150
-}
-
-
-$(".img class").on("click", function() { 
-  yourCharacter = ahsoka; 
-  //   // Show yourCharacter's name and healthPoints next to image
-  $(".section-title").append(yourCharacter["name"]);
-  $(".section-title").append(yourCharacter["healthPoints"]);
-  $("#enemies-to-attack").append($(".Rey"));
-  $("#enemies-to-attack").append($(".Kylo-Ren"));
-  $("#enemies-to-attack").append($(".Yoda"));
-  $(".section-title").append(yourCharacter["healthPoints"]);
-})
-
-
-$(".Ahsoka").on("click", function() {
-  if (ahsoka= yourCharacter) {
-    Rey = yourOpponent 
-    $("#defender-area").append($(".Rey"));
-    $(".your-oppnent").append(yourOpponent["name"])
-    $(".your-oppnent").append(yourOpponent[ "healthPoints"]);
-  }
-});
-
-$(".Kylo-Ren").on("click", function() {
-  yourCharacter = kyloRen; 
-  $("#enemies-to-attack").append($("#Ahsoka"));
-  $("#enemies-to-attack").append($(".Rey"));
-  $("#enemies-to-attack").append($(".Yoda"));
-});
-
-$(".Rey").on("click", function() {
-  yourCharacter = rey;
-  $("#enemies-to-attack").append($("#Ahsoka"));
-  $("#enemies-to-attack").append($(".Yoda")); 
-  $("#enemies-to-attack").append($(".Kylo-Ren"));
-});
-
-$(".Yoda").on("click", function() {
-  yourCharacter = yoda; 
-  $("#enemies-to-attack").append($("#Ahsoka"));
-  $("#enemies-to-attack").append($(".Kylo-Ren")); 
-  $("#enemies-to-attack").append($(".Rey"));
-});
-
-// place click function on character Class instead of on an individual character
-
-$(".img class").on("click", function() {
-   if (yourCharacter=== undefined) {
-
-    if (firstNumber === undefined) {
-      // sets value for the 1st number
-      firstNumber = $(this).val(); 
-      $("#first-number").text(firstNumber);  
+    let ahsoka = {
+      name: "Ahsoka",
+      healthPoints: 160,
+      attackPower:  9,
+      counterAttackPower:20
     }
-//     else { //concatenate firstNumber
-//       firstNumber = firstNumber + $(this).val() ;
-//       $("#first-number").text(firstNumber);
-//     }
-//   }
-//   else {
-//     if (secondNumber === undefined) {
-//       secondNumber = $(this).val()
-//       $("#second-number").text(secondNumber);
-//     } 
-//     else {
-//       secondNumber = secondNumber + $(this).val() ;
-//       $("#second-number").text(secondNumber);
-//     }
-//   }
+    
+    let kyloRen = {
+      name: "Kylo-Ren",
+      healthPoints: 150,
+      attackPower: 10,
+      counterAttackPower: 25
+    }
+    let rey = {
+      name: "Rey",
+      healthPoints: 170,
+      attackPower: 8,
+      counterAttackPower: 30
+    }
+    let yoda = {
+      name: "Yoda",
+      healthPoints: 175,
+      attackPower: 12,
+      counterAttackPower: 30
+    }
+  }
+}
+
+// $(".Ahsoka").on("click", function() {
+//   yourCharacter = ahsoka; 
+//   $("#enemies-to-attack").append($(".Rey"));
+//   $("#enemies-to-attack").append($(".Kylo-Ren"));
+//   $("#enemies-to-attack").append($(".Yoda"));
 // });
-<<<<<<< HEAD
-=======
 
+// $(".Kylo-Ren").on("click", function() {
+//   yourCharacter = kyloRen; 
+//   $("#enemies-to-attack").append($("#Ahsoka"));
+//   $("#enemies-to-attack").append($(".Rey"));
+//   $("#enemies-to-attack").append($(".Yoda"));
+// });
 
->>>>>>> fe687b41e7ad696217e53146cc46610fd094b873
+// $(".Rey").on("click", function() {
+//   yourCharacter = rey;
+//   $("#enemies-to-attack").append($("#Ahsoka"));
+//   $("#enemies-to-attack").append($(".Yoda")); 
+//   $("#enemies-to-attack").append($(".Kylo-Ren"));
+// });
+
+// $(".Yoda").on("click", function() {
+//   yourCharacter = yoda; 
+//   $("#enemies-to-attack").append($("#Ahsoka"));
+//   $("#enemies-to-attack").append($(".Kylo-Ren")); 
+//   $("#enemies-to-attack").append($(".Rey"));
+// });
+
